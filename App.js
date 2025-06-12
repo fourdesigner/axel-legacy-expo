a85d09ad56d31406ead3cca0eacc3b23faea7b67
fourdesigner <47359131+fourdesigner@users.noreply.github.com>
2025-06-12 18:09:18 -0400

    App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './Home';
+import Finance from './Finance';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Finance" component={Finance} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
